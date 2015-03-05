28916507637719683103e96afa4d0c657c02a78d
Michaeldean147 <michaeldean147@yahoo.com>
2015-03-05 15:35:59 -0800

    added show page for posts and ability to delete post for post's creator

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
 
   def new
     @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -17,6 +22,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
     if current_user.id == @post.user_id
       @post.destroy
       redirect_to posts_path

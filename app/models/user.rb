6369bfadf61c7f618483e676480a7ceca665d18f
Michaeldean147 <michaeldean147@yahoo.com>
2015-03-22 16:42:21 -0700

    added uniqueness to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :favorited_posts, through: :favorites, source: :post
 
   validates :username, presence: true
+  validates :username, uniqueness: true
   validates :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
